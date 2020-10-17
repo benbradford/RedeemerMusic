f50744cd8cdcb0cbeafaa6aec44ec893128eb6cc
Ben Bradford <bebradfo@amazon.com>
2020-10-17 08:51:49 +0100

    first check-in

---
+++ src/email_service.py
@@ -0,0 +1,39 @@
+from __future__ import print_function
+import pickle
+import os.path
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+class EmailService:
+
+    def __init__(self):
+        creds = self.create_credentials()
+        self.service = build('gmail', 'v1', credentials=creds)
+
+    def create_credentials(self):
+        creds = None
+        # The file token.pickle stores the user's access and refresh tokens, and is
+        # created automatically when the authorization flow completes for the first
+        # time.
+        if os.path.exists('token.pickle'):
+            with open('token.pickle', 'rb') as token:
+                creds = pickle.load(token)
+
+        if not creds or not creds.valid:
+            creds = self.login(creds)
+        return creds
+
+    def login(self, creds):
+        SCOPES = ['https://www.googleapis.com/auth/gmail.readonly']
+
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                '../cred/credentials_gmail.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+        return creds
---
+++ src/main.py
@@ -0,0 +1,18 @@
+from email_service import EmailService
+
+def main():
+    service = EmailService().service
+
+    # Call the Gmail API
+    results = service.users().labels().list(userId='me').execute()
+    labels = results.get('labels', [])
+
+    if not labels:
+        print('No labels found.')
+    else:
+        print('Labels:')
+        for label in labels:
+            print(label['name'])
+
+if __name__ == '__main__':
+    main()
