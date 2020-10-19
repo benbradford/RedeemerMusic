e01755460c71f292a5ddf6d323eefc003ee88e4c
Ben Bradford <bebradfo@amazon.com>
2020-10-19 23:02:01 +0100

    Driving via command line args

--- src/create_group_email.py
+++ src/create_group_email.py
@@ -1,16 +1,13 @@
-from song_item import SongItem
 from song_items import SongItems
 from email_template import EmailTemplate
-from member import Member
 from members import Members
 from email_service import EmailService
 import base64
 import json
+import sys, getopt
 
-from email.mime.text import MIMEText
-
-all_song_items = SongItems('../res/songs.json')
-all_members = Members('../res/members.json')
+all_song_items = SongItems('res/songs.json')
+all_members = Members('res/members.json')
 
 def get_songs(params):
     songs = []
@@ -24,7 +21,7 @@ def get_band(params):
         band.append(all_members.get_by_id(member))
     return band
 
-def main(param_filename):
+def main(param_filename, email_from_file, email_to_file):
 
     params = json.loads(open(param_filename, "r").read())
 
@@ -35,11 +32,11 @@ def main(param_filename):
     extra = params['extra']
 
     template = EmailTemplate(date, songs, band, lead, extra)
-    email_from = base64.urlsafe_b64encode(open('../res/email_from.txt', "r").read())
-    email_to = open('../res/email_recipients.txt', "r").readline()[:-1]
+    email_from = base64.urlsafe_b64encode(open(email_from_file, "r").read())
+    email_to = open(email_to_file, "r").readline()[:-1]
     print('Will send to {}'.format(email_to))
-    print('email body can be checked in ../bin/email_output.html')
-    f = open("../bin/email_output.html", "w")
+    print('email body can be checked in bin/email_output.html')
+    f = open("bin/email_output.html", "w")
     f.write(template._html)
     f.close()
     proceed_input = raw_input("Ok to proceed? (y/n): ")
@@ -50,4 +47,20 @@ def main(param_filename):
         EmailService().send(raw_message)
 
 if __name__ == '__main__':
-    main('../res/email_param_template.json')
+    argv = sys.argv[1:]
+    service_file = 'services/test_service.json'
+    email_from_file = 'res/email_from.txt'
+    email_to_file = 'res/email_recipients.txt'
+    try:
+        opts, args = getopt.getopt(argv, "s:f:t:",["service=","from=","to="])
+    except getopt.GetoptError:
+        print 'create_group_email.py -s <service_file> -f <email_from_file> -t <email_to_file>'
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt in ('-s', '--service'):
+            service_file = arg
+        elif opt in ('-f', '--from'):
+            email_from_file = arg
+        elif opt in ('-t', '--to'):
+            email_to_file = arg
+    main(service_file, email_from_file, email_to_file)
--- src/email_template.py
+++ src/email_template.py
@@ -5,7 +5,7 @@ from email.mime.multipart import MIMEMultipart
 
 class EmailTemplate:
     def __init__(self, date, song_items, members, lead, extra):
-        self._html = open('../res/email_template.html', "r").read()
+        self._html = open('res/email_template.html', "r").read()
         html_song_list = self._make_html_song_list(song_items)
         html_member_list = self._make_html_members_list(members, lead)
         self._html = self._html\
@@ -15,13 +15,11 @@ class EmailTemplate:
         .replace('_EXTRA_', extra)
 
     def create_message(self, date, _from, to):
-
         message = MIMEMultipart()
         message.attach(MIMEText(self._html, "html"))
         message['to'] = to
         message['from'] = _from
         message['subject'] = 'Redeemer Music for {}'.format(date)
-
         return message
 
     def _make_html_song_list(self, song_items):
