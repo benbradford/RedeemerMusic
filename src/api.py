187211ccb89ecc1f53e459ebe2fad9d4265620d7
Ben Bradford <bebradfo@amazon.com>
2021-03-22 21:32:47 +0000

    ability to add and update services

--- src/api.py
+++ src/api.py
@@ -75,6 +75,20 @@ def _write_members(members):
     with open('../res/members.json', 'w+') as f:
         json.dump(members, f, indent=4, sort_keys=True)
 
+def _validate_service(service):
+    if 'band' not in service:
+        return "Error: no band"
+    if 'date' not in service:
+        return "Error: no date"
+    if 'lead' not in service:
+        return "Error: no lead"
+    if 'songs' not in service:
+        return "Error: no songs"
+    for song in service['songs']:
+        if _get_song(song)[1] == -1:
+            return "Error: cannot find song " + str(song)
+    return None
+
 app = flask.Flask(__name__)
 CORS(app)
 app.config["DEBUG"] = True
@@ -138,6 +152,52 @@ def send_powerpoint():
     result = {}
     result['res'] = songs_to_use
     return jsonify(result)
+
+@app.route('/add_service', methods=['POST'])
+def add_service():
+    songs = _get_songs()
+    new_service = request.get_json(force=True)
+    if new_service is None:
+        return "Error: no json body supplied"
+    if 'key' in new_service:
+        return "Error: id should not be supplied"
+
+    error = _validate_service(new_service)
+    if error:
+        return error
+
+    services = _get_services()
+    new_service['id'] = len(services['services'])
+    services['services'].append(new_service)
+    _write_services(services)
+    return jsonify(services)
+
+@app.route('/update_service', methods=['POST'])
+def update_service():
+    songs = _get_songs()
+    new_service = request.get_json(force=True)
+    if new_service is None:
+        return "Error: no json body supplied"
+    if 'id' not in new_service:
+        return "Error: no id supplied"
+
+    error = _validate_service(new_service)
+    if error:
+        return error
+
+    services = _get_services()
+    index = -1
+    for i, service in enumerate(services['services']):
+        if service['id'] == new_service['id']:
+            index = i
+            break
+    if index == -1:
+        return "Error: could not find service"
+
+    services['services'].pop(index)
+    services['services'].append(new_service)
+    _write_services(services)
+    return jsonify(services)
 
 @app.route('/add_song', methods=['POST'])
 def add_song():
